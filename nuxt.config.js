f24b501de52e602354b3ec831193b6df5470cf36
Maysa <maysa.bonfante@webcore.com.br>
2024-11-05 09:09:47 -0300

    vuetify config + sass

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,34 @@
+import vuetify from 'vite-plugin-vuetify'
+
 export default defineNuxtConfig({
+	compatibilityDate: '2024-07-11',
+
     modules: [
-		'nuxt-swiper'
+		'nuxt-swiper',
+		async (options, nuxt) => {
+			nuxt.hooks.hook('vite:extendConfig', (config) =>
+				config.plugins.push(vuetify())
+			)
+		},
 	],
+
+	css: ['vuetify/lib/styles/main.sass'],
+
+	build: {
+		transpile: ['vuetify'],
+	  },
+
+	vite: {
+		styles: {
+			configFile: 'src/styles/settings.scss',
+		},
+		css: {
+			preprocessorOptions: {
+				scss: {
+					additionalData: '@use "@/assets/scss/variables.scss" as *;',
+					api: 'modern-compiler',
+				},
+			},
+		},
+	},
 })
