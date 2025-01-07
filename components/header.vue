d147e04cc77803979d20af7c536329e7f44d66d4
Maysa-B <maybsalvalaio@gmail.com>
2025-01-07 14:44:46 -0300

    submenu logic

--- components/header.vue
+++ components/header.vue
@@ -18,22 +18,29 @@
                 </v-row>
             </swiper-slide>
         </swiper>
-        <v-app-bar scroll-behavior="hide elevate" scroll-threshold="290" :class="`${ showHero ? 'show' : ''} ${ color }`" id="app-header">
-            <v-spacer></v-spacer>
-            <div class="d-flex justify-space-between align-center w-25">
-                <nuxt-link v-for="m, i in menu" :key="`menu-${i}`" to="#">{{ m.title }}</nuxt-link>
-            </div>
-            <v-spacer></v-spacer>
-            <img  width="50px" src="https://www.starbucksathome.com/br/static/version1730292575/frontend/Webjump/starbucks/pt_BR/images/logo.svg"></img>
-            <v-spacer></v-spacer>
-            <nav class="d-flex justify-space-between align-center w-25">
-                <v-btn variant="outlined" color="white">Compre aqui</v-btn>
-                <nuxt-link to="#"><svg width="25px" height="25px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M15.7955 15.8111L21 21M18 10.5C18 14.6421 14.6421 18 10.5 18C6.35786 18 3 14.6421 3 10.5C3 6.35786 6.35786 3 10.5 3C14.6421 3 18 6.35786 18 10.5Z" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg></nuxt-link>
-                <nuxt-link to="#"><svg width="25px" height="25px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M6.29977 5H21L19 12H7.37671M20 16H8L6 3H3M9 20C9 20.5523 8.55228 21 8 21C7.44772 21 7 20.5523 7 20C7 19.4477 7.44772 19 8 19C8.55228 19 9 19.4477 9 20ZM20 20C20 20.5523 19.5523 21 19 21C18.4477 21 18 20.5523 18 20C18 19.4477 18.4477 19 19 19C19.5523 19 20 19.4477 20 20Z" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg></nuxt-link>
-                <nuxt-link to="#">Entrar</nuxt-link>
-                <nuxt-link to="#">Registrar</nuxt-link>
-            </nav>
-            <v-spacer></v-spacer>
+        <v-app-bar @mouseout="closeMenu" scroll-behavior="hide elevate" scroll-threshold="290" :class="`${ showHero ? 'show' : ''} ${ color }`" id="app-header">
+            <v-row class="menu">
+                <v-spacer></v-spacer>
+                <div class="d-flex justify-space-between align-center w-25">
+                    <nuxt-link @mouseover="handleDesktopMenuItemClick($event, m)" v-for="m, i in menu" :key="`menu-${i}`" to="#">{{ m.title }}</nuxt-link>
+                </div>
+                <v-spacer></v-spacer>
+                <img  width="50px" src="https://www.starbucksathome.com/br/static/version1730292575/frontend/Webjump/starbucks/pt_BR/images/logo.svg"></img>
+                <v-spacer></v-spacer>
+                <nav class="d-flex justify-space-between align-center w-25">
+                    <v-btn variant="outlined" color="white">Compre aqui</v-btn>
+                    <nuxt-link to="#"><svg width="25px" height="25px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M15.7955 15.8111L21 21M18 10.5C18 14.6421 14.6421 18 10.5 18C6.35786 18 3 14.6421 3 10.5C3 6.35786 6.35786 3 10.5 3C14.6421 3 18 6.35786 18 10.5Z" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg></nuxt-link>
+                    <nuxt-link to="#"><svg width="25px" height="25px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M6.29977 5H21L19 12H7.37671M20 16H8L6 3H3M9 20C9 20.5523 8.55228 21 8 21C7.44772 21 7 20.5523 7 20C7 19.4477 7.44772 19 8 19C8.55228 19 9 19.4477 9 20ZM20 20C20 20.5523 19.5523 21 19 21C18.4477 21 18 20.5523 18 20C18 19.4477 18.4477 19 19 19C19.5523 19 20 19.4477 20 20Z" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg></nuxt-link>
+                    <nuxt-link to="#">Entrar</nuxt-link>
+                    <nuxt-link to="#">Registrar</nuxt-link>
+                </nav>
+                <v-spacer></v-spacer>
+            </v-row>
+            <v-row v-if="openSubmenu" class="submenu">
+                <div v-for="s, i in selectedItem?.submenu" :key="`submenu-${i}`">
+                    <p>{{ s.title }}</p>
+                </div>
+            </v-row>
         </v-app-bar>
     </div>
 </template>
@@ -99,9 +106,27 @@
     ]
 
     const color = ref('green')
-    const showNavbar = ref(true)
     const showHero = ref(true)
+    const selectedItem = ref({})
+    const openSubmenu = ref(false)
     let lastScrollPosition = ref(0)
+
+    const handleDesktopMenuItemClick = (e, item) => {
+        if(item?.submenu){
+            e.preventDefault()
+
+            if(toRaw(selectedItem.value) === item)
+                openSubmenu.value = true
+            else {
+                if(openSubmenu.value === false)
+                    openSubmenu.value = true
+
+                selectedItem.value = toValue(item)
+            }
+        } else {
+            openSubmenu.value = false
+        }
+    }
 
     function onScroll () {
         const currentScrollPosition = window.pageYOffset || document.documentElement.scrollTop
@@ -123,6 +148,11 @@
         }
        
         lastScrollPosition.value = currentScrollPosition
+    }
+
+    const closeMenu = (e) => {
+        if(!e.currentTarget.contains(e.relatedTarget))
+            openSubmenu.value = false
     }
 
     onMounted(() => {
